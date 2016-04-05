ae9f9182f975dd52203784d77532fda47b09d446
stan wielga <svwielga4@gmail.com>
2016-04-05 10:46:01 -0700

    creating basic layout

--- index.php
+++ index.php
@@ -2,10 +2,27 @@
     <html>
         <head>
             <title>Memory Match</title>
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
             <link href="style.css" type="text/css" rel="stylesheet"/>
             <script src="script.js"></script>
         </head>
         <body>
-            
+            <div class="header">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>High Score</li>
+                </ul>
+            </div>
+            <div class="scoreboard">
+                <ul>
+                    <li>Timer</li>
+                    <li>Score</li>
+
+                </ul>
+            </div>
+            <div class="game_area">
+                
+            </div>
         </body>
     </html>
