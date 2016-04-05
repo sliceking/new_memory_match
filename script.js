6d7f6bb6a32421dc9f638d159980d859d96aa6f2
stan wielga <svwielga4@gmail.com>
2016-04-05 12:12:27 -0700

    added cards appending rough

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 $(document).ready(function(){
-    
+    append_card_backs();
 
 });
 
+function append_card_backs(){
+    for (var i=0;i<18;i++){
+        var card_fronts=['images/bill.png','images/bill.png','images/dipper.png','images/dipper.png','images/ford.png','images/ford.png','images/gideon.png','images/gideon.png','images/gnome.png','images/gnome.png','images/mabel.jpg','images/mabel.jpg','images/soos.png','images/soos.png','images/unclestan.png','images/unclestan.png','images/wendy.png','images/wendy.png'];
+        var game_area = $('.game_area');
+        var card_div = $('<div>',{
+            class:'card'
+        });
+        var card_back = $('<img>',{
+            src:'images/cardBack.jpg'
+        });
+        var card_front = $('<img>',{
+            src:card_fronts[i],
+            class:'card_front'
+        })
+        $(card_div).append(card_back);
+        $(card_div).append(card_front);
+        $(game_area).append(card_div);
+    }
+}
