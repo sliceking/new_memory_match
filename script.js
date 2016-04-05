b82ec51586e2d8c6f076793511429c1dc9b14be4
stan wielga <svwielga4@gmail.com>
2016-04-05 15:22:29 -0700

    compare cards works

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+var first_card=null;
+var second_card=null;
 $(document).ready(function(){
     append_card_backs();
 
+    $('.card_back').click(function(){
+        show_card(this);
+    })
 });
 
 function append_card_backs(){
@@ -11,14 +16,38 @@ function append_card_backs(){
             class:'card'
         });
         var card_back = $('<img>',{
-            src:'images/cardBack.jpg'
+            src:'images/cardBack.jpg',
+            class:'card_back'
         });
         var card_front = $('<img>',{
             src:card_fronts[i],
             class:'card_front'
-        })
+        });
         $(card_div).append(card_front);
         $(card_div).append(card_back);
         $(game_area).append(card_div);
+    }
+}
+function show_card(card){
+    var inside = $(card).prev().attr('src');
+    console.log(inside);
+    $(card).hide();
+    console.log('show card fired');
+    compare_cards(inside);
+}
+function compare_cards(card){
+    if (first_card == null){
+        first_card = card;
+    }else if(second_card == null) {
+        second_card = card;
+        if (first_card == second_card) {
+            console.log('cards match');
+            first_card = null;
+            second_card = null;
+        } else {
+            console.log('cards dont match');
+            first_card = null;
+            second_card = null;
+        }
     }
 }
